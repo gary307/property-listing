7d3a4fd29aa81bc928cc10716a2e5a3eab57070b
Gary Butler <garybutler@192.168.1.163>
2024-07-07 11:06:35 +0100

    update: added list

--- src/components/PropertyListing/tests/PropertyListing.spec.jsx
+++ src/components/PropertyListing/tests/PropertyListing.spec.jsx
@@ -3,9 +3,87 @@ import { render, screen } from '@testing-library/react';
 import { within } from '@testing-library/dom';
 import PropertyListing from '../PropertyListing';
 
+const DUMMY_PROPERTY = [{
+    id: 73864112,
+    bedrooms: 3,
+    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
+    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
+    propertyType: 'Flat',
+    price: 1950000,
+    branchName: 'M2 Property, London',
+    propertyUrl: '/property-for-sale/property-73864112.html',
+    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
+    propertyTitle: '3 bedroom flat for sale',
+    mainImage:
+        'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
+},
+{
+    id: 73864112,
+    bedrooms: 3,
+    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
+    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
+    propertyType: 'Flat',
+    price: 1950000,
+    branchName: 'M2 Property, London',
+    propertyUrl: '/property-for-sale/property-73864112.html',
+    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
+    propertyTitle: '3 bedroom flat for sale',
+    mainImage:
+        'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
+},{
+    id: 73864112,
+    bedrooms: 3,
+    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
+    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
+    propertyType: 'Flat',
+    price: 1950000,
+    branchName: 'M2 Property, London',
+    propertyUrl: '/property-for-sale/property-73864112.html',
+    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
+    propertyTitle: '3 bedroom flat for sale',
+    mainImage:
+        'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
+},{
+    id: 73864112,
+    bedrooms: 3,
+    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
+    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
+    propertyType: 'Flat',
+    price: 1950000,
+    branchName: 'M2 Property, London',
+    propertyUrl: '/property-for-sale/property-73864112.html',
+    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
+    propertyTitle: '3 bedroom flat for sale',
+    mainImage:
+        'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
+},{
+    id: 73864112,
+    bedrooms: 3,
+    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
+    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
+    propertyType: 'Flat',
+    price: 1950000,
+    branchName: 'M2 Property, London',
+    propertyUrl: '/property-for-sale/property-73864112.html',
+    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
+    propertyTitle: '3 bedroom flat for sale',
+    mainImage:
+        'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
+}];
+
+
+global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(DUMMY_PROPERTY),
+    })
+  );
+
 describe('PropertyListing', () => {
     it('should render five property cards', async () => {
         render(<PropertyListing />);
+
+        screen.debug();
+
         const propertiesList = screen.getByRole('list');
         const propertyCards = await within(propertiesList).findAllByRole('listitem');
         expect(propertyCards).toHaveLength(5);
